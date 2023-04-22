f7f64322b09da68b97ab2a053d08eaf7c14867b0
Aniket <kumaraniket900@gmail.com>
2023-04-22 11:42:33 +0530

    import swing removed

--- src/main/java/com/railway/tracker/RailwayTrackerApplication.java
+++ src/main/java/com/railway/tracker/RailwayTrackerApplication.java
@@ -3,7 +3,6 @@ package com.railway.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
 
 @SpringBootApplication
 public class RailwayTrackerApplication {
