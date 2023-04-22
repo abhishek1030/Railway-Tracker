d40aafc7ad9d18f8a20ea8a5be42e47ed417388f
Aniket <kumaraniket900@gmail.com>
2023-04-22 11:56:55 +0530

    updated main class

--- src/main/java/com/railway/tracker/RailwayTrackerApplication.java
+++ src/main/java/com/railway/tracker/RailwayTrackerApplication.java
@@ -9,6 +9,7 @@ public class RailwayTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RailwayTrackerApplication.class, args);
 		System.out.println("RailwayTrackerApplication started !!");
+		System.out.println("RailwayTrackerApplication started !! 333");
 		System.out.println("Railway Tracker is running ...... ");
 
 	}
