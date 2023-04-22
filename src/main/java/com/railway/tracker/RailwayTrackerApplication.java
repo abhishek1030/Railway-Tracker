1f34567fad1af2e1b843681f514d4aa9c5886f5b
Aniket <kumaraniket900@gmail.com>
2023-04-22 11:31:35 +0530

    sout added in main method

--- src/main/java/com/railway/tracker/RailwayTrackerApplication.java
+++ src/main/java/com/railway/tracker/RailwayTrackerApplication.java
@@ -3,11 +3,15 @@ package com.railway.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class RailwayTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RailwayTrackerApplication.class, args);
+		System.out.println("RailwayTrackerApplication started !!");
+
 	}
 
 }
