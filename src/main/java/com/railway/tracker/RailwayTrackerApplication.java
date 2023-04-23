9211429ab62019885dfbe7c4ef29f0303507f767
Abhishek Anand <anand007abhishek@gmail.com>
2023-04-23 09:09:03 +0530

    Initial setup testing

--- src/main/java/com/railway/tracker/RailwayTrackerApplication.java
+++ src/main/java/com/railway/tracker/RailwayTrackerApplication.java
@@ -9,9 +9,6 @@ public class RailwayTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RailwayTrackerApplication.class, args);
 		System.out.println("RailwayTrackerApplication started !!");
-		System.out.println("RailwayTrackerApplication started !! 333");
-		System.out.println("Railway Tracker is running ...... ");
-		//Checking all the git issues
 	}
 
 }
